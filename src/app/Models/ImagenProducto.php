f02ef8e1febe71e38cc6accc18413cb365f9ea9f
= <=>
2021-01-06 17:44:24 -0500

    Crud de producto sin Imagen

---
+++ src/app/Models/ImagenProducto.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ImagenProducto extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'path',
+        'producto_id'
+    ];
+
+    public function producto()
+    {
+        return $this->belongsTo(Producto::class);
+    }
+}
